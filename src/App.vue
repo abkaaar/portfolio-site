b4e8a5328a87973681faa924fa045b38ecaeb3d5
abdull461 <abdullahabubakar461@gmail.com>
2023-10-16 17:38:17 +0100

    deploy

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+
+  <header>
+    <nav class="bg-white dark:bg-gray-900 w-full z-20 top-0 left-0">
+  <div class="max-w-screen-xl flex justify-between items-center justify-center mx-auto p-2" id="na">
+    <RouterLink to="/" class="">
+
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white sm:hidden" id="ull">AA</span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white " id="full">Abubakar Abdullahi</span>
+      </RouterLink>
+  <div class="items-center justify-between  w-full md:flex md:w-auto md:order-1" id="">
+    
+    <ul class="flex justify-center p-4   font-medium border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+     
+        <RouterLink to="/about">About Me</RouterLink>
+       
+    </ul>
+  </div>
+  </div>
+</nav>
+    <div class="wrapper">
+      <!-- <HelloWorld msg="You did it!" /> -->
+    </div>
+  </header>
+  <RouterView />
+  <footer class="flex justify-center">
+    <ul class="flex gap-4 justify-center">
+    
+      <li><a href="#"><img src="../src/assets/static/linkedin.png" alt="linkedin"></a></li>
+      <li><a href="#"><img src="../src/assets/static/github.png" alt="github"></a></li>
+      <li><a href="#"><img src="../src/assets/static/twitter.png" alt="twitter"></a></li>
+      <li><a href="#"><img src="../src/assets/static/instagram.png" alt="instagram"></a></li>
+      <li><a href="#"><img src="../src/assets/static/kaggle.png" alt="kaggle"></a></li>
+    </ul>
+  </footer>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Kaushan+Script&family=Pacifico&display=swap');
+
+/* font-family: 'Dancing Script', cursive;
+font-family: 'Kaushan Script', cursive;
+font-family: 'Pacifico', cursive; */
+@import url('https://fonts.googleapis.com/css2?family=Libre+Bodoni:wght@400;500&display=swap');
+
+
+*,:after,:before{
+  border: none;
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+ 
+}
+header{
+  font-family: 'Libre Bodoni', serif;
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    /* text-align: left;
+    margin-left: -1rem; */
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 0.5rem;
+  }
+}
+@media (min-width: 375px) {
+ul {
+  justify-content: end;
+}}
+@media (min-width: 500px){
+  	#ull{
+      display: none;
+    }
+}
+@media (max-width: 500px) {
+  #full{
+  display: none;
+}
+}
+</style>
---
+++ src/views/project.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="header m-10">
+    <h1 class="mb-4 text-4xl tracking-tight leading-none text-center md:text-5xl lg:text-6xl">
+      Projects
+    </h1>
+
+    <p class="mt-8 text-center">A sample of my work</p>
+  </div>
+
+  <div class="project flex justify-center items-center">
+    <div class="grid grid-cols-2 max-md:grid-cols-1 p-8 gap-8">
+      <div class="box text-center text-white flex justify-center rounded-lg">
+        <img
+          class="h-auto max-w-md rounded-lg scale-100"
+          src="../assets/projects/migrants.jpg"
+          alt=""
+        />
+        <div class="box-content">
+          <h3 class="title"><strong>Personal project</strong></h3>
+          <a
+            target="_blank"
+            href="https://app.powerbi.com/view?r=eyJrIjoiYmU1NjVjNTMtODc4My00NjYyLWJjYTItZGIzM2UwOWU5ODY1IiwidCI6ImM3NmZjOWRjLTc0OTQtNDhhOC1hYzFmLTg1YTJmZTI0ZWQzZiJ9"
+            class="post"
+            >Analysis on Global Dead & missing Migrants 2014-2015</a
+          >
+        </div>
+      </div>
+      <div class="box rounded-lg">
+        <img class="h-auto max-w-md rounded-lg" src="../assets/projects/bar3.png" alt="" />
+        <div class="box-content">
+          <h3 class="title"><strong>Personal project</strong></h3>
+          <a
+            target="_blank"
+            href="https://github.com/abdullah461/Analysis-on-enviromental-impact-of-food-production.git"
+            class="post"
+            >Analysis on enviromental impact of food production</a
+          >
+        </div>
+      </div>
+      <div class="box rounded-lg">
+        <img class="h-auto max-w-md rounded-lg" src="../assets/projects/un.jpeg" alt="" />
+        <div class="box-content">
+          <h3 class="title"><strong>Personal project</strong></h3>
+          <a target="_blank" href="" class="post"
+            >Analysis on Unemployment in Nigeria from 2015 to 2021</a
+          >
+        </div>
+      </div>
+
+      <div class="box rounded-lg">
+        <img
+          class="h-auto max-w-md rounded-lg"
+          src="../assets/projects/common-wealth.jpeg"
+          alt=""
+        />
+        <div class="box-content">
+          <h3 class="title"><strong>Personal project</strong></h3>
+          <a target="_blank" href="https://github.com/abdullah461/Common-wealth-games.git" class="post"
+            >Analysis on common wealth games 2022 medals</a
+          >
+        </div>
+      </div>
+      <div class="box rounded-lg">
+        <img class="h-auto max-w-md rounded-lg" src="../assets/projects/sales.png" alt="" />
+        <div class="box-content">
+          <h3 class="title"><strong>Personal project</strong></h3>
+          <a target="_blank" href="" class="post"
+            >Analysis on Unemployment in Nigeria from 2015 to 2021</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Kaushan+Script&family=Montserrat&family=Pacifico&display=swap');
+
+/* @media (min-width: 1024px) {
+  .project {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+.header h1 {
+  color: #5988ff;
+}
+.post:hover {
+  text-decoration: underline;
+}
+.box {
+  color: #fff;
+  background: linear-gradient(to right, #5988ff, #ff6600);
+  font-family: 'Montserrat', sans-serif;
+  text-align: center;
+  position: relative;
+  z-index: 1;
+}
+.box:before {
+  content: '';
+  height: 40px;
+  width: 40px;
+  border-top: 1px solid #fff;
+  border-right: 1px solid #fff;
+  opacity: 0;
+  transform: scale(0);
+  transform-origin: top right;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  transition: all 0.4s ease;
+  z-index: 1;
+}
+.box:hover:before {
+  opacity: 1;
+  transform: scale(1);
+}
+.box img {
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  transition: all 0.4s ease 0s;
+}
+.box:hover img {
+  opacity: 0.2;
+}
+.box-content {
+  width: 100%;
+  opacity: 0;
+  transform: translateX(-50%) translateY(-50%);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 2;
+  transition: all 0.4s ease;
+}
+.box:hover .box-content {
+  opacity: 1;
+}
+.box .icon {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  opacity: 0;
+  position: absolute;
+  bottom: 10px;
+  left: 20px;
+  z-index: 2;
+  transition: all 0.4s ease;
+}
+.box:hover .icon {
+  opacity: 1;
+}
+.box .icon li {
+  margin: 0 3px;
+  display: inline-block;
+}
+.box .icon li a {
+  color: #fff;
+  font-size: 15px;
+  text-align: center;
+  line-height: 32px;
+  height: 35px;
+  width: 35px;
+  border: 2px solid #fff;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.6);
+  display: block;
+  transition: all 0.3s ease-in-out;
+}
+.box .icon li a:hover {
+  color: #ff6600;
+  background: #fff;
+  transform: rotate(360deg);
+  border-radius: 50px;
+}
+</style>
